2ca547a7bd63079ebf573b0a5495446c9e449e8e
Jack Zhao <jack@plasticmobile.com>
2016-06-08 11:54:45 -0400

    First Commit

---
+++ InsertionSort/InsertionSort.playground/Contents.swift
@@ -0,0 +1,45 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+/**
+ * My Implementation
+ */
+func insertionSort(array:[Int]) -> [Int] {
+    var sortedArray = [Int]()
+    
+    for i in 0...array.count-1 {
+        if sortedArray.count == 0 {
+            sortedArray.append(array[i])
+        } else {
+            for j in 0...sortedArray.count-1 {
+                if sortedArray[j] >= array[i] {
+                    sortedArray.insert(array[i], atIndex: j)
+                    break
+                } else if j == sortedArray.count - 1 {
+                    sortedArray.append(array[i])
+                }
+            }
+        }
+    }
+    
+    return sortedArray
+}
+
+func sampleInsertionSort(array:[Int]) -> [Int] {
+    var a = array
+    for x in 1..<array.count {
+        var y = x
+        while y>0 && a[y]<a[y-1] {
+            swap(&a[y-1], &a[y])
+            y-=1
+        }
+    }
+    return a
+}
+
+
+let arrayToSort = [2,20,3,30,20,-9]
+
+insertionSort(arrayToSort)
+
+sampleInsertionSort(arrayToSort)
